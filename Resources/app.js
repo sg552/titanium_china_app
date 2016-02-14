4e6f35be22d3ce2f28e92f8d59a03118614d4d7d
Shen Siwei <shensiwei@sina.com>
2016-02-15 07:13:31 +0800

    实现了显示 forums 列表

--- Resources/app.js
+++ Resources/app.js
@@ -5,13 +5,19 @@ var window = Ti.UI.createWindow({});
 var http = Ti.Network.createHTTPClient({
   onload: function(e){
     var response = JSON.parse(this.responseText);
+
+    // 设置好data的格式。
     var data = []
     for(i = 0; i < response.length; i++){
       data.push({
         forum_name: { text: response[i].name},
-        forum_description: { text: response[i].description }
+        forum_description: { text: response[i].description },
+        // 这个forum_id 不需要放到ListView中，
+        // 所以不需要再设置一个 { text: ... }
+        forum_id: response[i].id
       });
     }
+    // 把data 作为参数，传递到 forum.js 中去。
     require('forums')(data).open();
   },
   onerror: function(e){
